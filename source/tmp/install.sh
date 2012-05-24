3c50c1f46f134e54f7f923b94fc5b10fad929474
Peter Donka <vadonka@vadonka.com>
2012-05-24 15:02:47 +0200

    Cleanup

--- source/tmp/install.sh
+++ source/tmp/install.sh
@@ -171,7 +171,7 @@ else
 fi
 ui_print "-building the new kernel..."
 $chmod 0777 $bd/mkbootimg
-$bd/mkbootimg --kernel $bd/zImage --ramdisk $bd/initrd.img --cmdline "mem=$((383+$hack))M@0M nvmem=$((128-$hack))M@$((384+$hack))M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990" -o $bd/boot.img --base 0x10000000
+$bd/mkbootimg --kernel $bd/zImage --ramdisk $bd/initrd.img --cmdline "mem=$((383+$hack))M@0M nvmem=$((128-$hack))M@$((384+$hack))M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990 avpfreq=240000 gpufreq=300000 vdefreq=650000" -o $bd/boot.img --base 0x10000000
 if [ "$?" -ne 0 -o ! -f $bd/boot.img ]; then
     fatal "ERROR: Packing kernel failed!"
 else
